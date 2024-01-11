ebe4c745bc4323408d6bc698956456c13aa204fa
Alex Isaacs <alexisaacs18@gmail.com>
2024-01-11 10:03:22 -0500

    added delete

--- server/app.py
+++ server/app.py
@@ -1,5 +1,5 @@
 from config import app
-from flask import make_response
+from flask import make_response, request
 
 from models import db, Companies, Open_Positions, Contact, Linkedin
 
@@ -17,11 +17,13 @@ def companies():
 
     return response
 
+
 @app.route("/open_positions", methods=["GET"])
 def open_positions():
     open_positions = Open_Positions.query.all()
 
-    open_positions_dict = [open_position.to_dict() for open_position in open_positions]
+    open_positions_dict = [open_position.to_dict()
+                           for open_position in open_positions]
 
     response = make_response(
         open_positions_dict,
@@ -29,6 +31,7 @@ def open_positions():
     )
 
     return response
+
 
 @app.route("/contacts", methods=["GET"])
 def contact():
@@ -43,18 +46,51 @@ def contact():
 
     return response
 
+
 @app.route("/linkedin", methods=["GET"])
 def linkedin():
     linkedin_for_contacts = Linkedin.query.all()
 
-    linkedin_dict = [contact_linkedin.to_dict() for contact_linkedin in linkedin_for_contacts]
+    linkedin_dict = [contact_linkedin.to_dict()
+                     for contact_linkedin in linkedin_for_contacts]
 
     response = make_response(
         linkedin_dict,
         200
     )
-    
+
     return response
-    
+
+
+@app.route("/companies/<int:id>", methods=["GET", "DELETE"])
+def companies_id(id):
+    companies_id = Companies.query.filter(Companies.id == id).first()
+
+    if companies_id:
+        if request.method == "GET":
+            companies_id_dict = companies_id.to_dict()
+
+            response = make_response(
+                companies_id_dict,
+                200
+            )
+
+        elif request.method == "DELETE":
+            db.session.delete(companies_id)
+            db.session.commit()
+
+            response = make_response(
+                {},
+                202
+            )
+    else:
+        response = make_response(
+            {"error": "ID does not exist"},
+            404
+        )
+
+    return response
+
+
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
